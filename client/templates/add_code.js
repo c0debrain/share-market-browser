d72861f99368ab97b11973a6a94962dae49e1a77
Patricio Soto Kort <psotokort@gmail.com>
2016-06-05 09:57:08 +0800

    Add and Edit Companies as Code is ID

--- client/templates/add_code.js
+++ client/templates/add_code.js
@@ -2,5 +2,11 @@ Template.addCode.onRendered(function(){
     $('#addcode-panel-content').collapse('toggle');
 });
 
-
-
+Template.addCode.events({
+    'change [name="code"], keyup [name="code"]' (event) {
+        //console.log(event.target.value);
+        if(Codes.findOne({_id:event.target.value})) {
+            console.log('Exists');
+        }
+    }
+});
